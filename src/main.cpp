6f36771c619c6519e8ae4cc7b0b73fb79fc8ec05
Maddison Wells <maddison.wells90@gmail.com>
2025-08-03 20:09:28 -0600

    updated read me

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,24 @@
-// #include <SFML/Graphics.hpp>
-
-// int main ()
-// {
-//   sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
-
-//   return 0;
-// }
+// // make
+// // ./dcc-app 
 
 #include <SFML/Graphics.hpp>
-
-// the following are included transitively in <SFML/Graphics.hpp>
-// #include <SFML/System.hpp>
-// #include <SFML/Window.hpp>
+#include <optional>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML with Homebrew Setup Guide");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Blue);
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Dungeon Crawler Carl");
 
-  while (window.isOpen()) {
-    while (const std::optional event = window.pollEvent())
-      if (event->is<sf::Event::Closed>())
-        window.close();
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) { 
+                window.close();
+            }
+        }
 
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
+        window.clear(sf::Color::Yellow);
+        // drawing code goes here, shouldnt update here
+        
+        window.display();
+    }
 
-  return 0;
+    return 0;
 }
