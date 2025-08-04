2c7a009422840f34055040c75dbdb699a50f86fd
Maddison Wells <maddison.wells90@gmail.com>
2025-08-03 21:11:57 -0600

    shapes tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,30 @@
 #include <optional>
 
 int main() {
+
+//----------------INITIALISE---------    
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Dungeon Crawler Carl");
+    sf::CircleShape shape(50.0f); //f is floating point
+    shape.setPosition(sf::Vector2f(100,100));
+    //----------------INITIALISE---------    
 
     while (window.isOpen()) {
+//----------------UPDATE---------    
+
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) { 
                 window.close();
             }
         }
 
-        window.clear(sf::Color::Yellow);
+//----------------UPDATE---------  
+
+//----------------DRAW---------  
+        window.clear(sf::Color::Black);
         // drawing code goes here, shouldnt update here
-        
+        window.draw(shape);
         window.display();
+//----------------DRAW---------  
     }
 
     return 0;
