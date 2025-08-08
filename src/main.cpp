0a180e34aff3105d5885d32c2d1488e0f336fdb4
Maddison Wells <maddison.wells90@gmail.com>
2025-08-08 14:36:24 -0600

    projectiles create and fire

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,21 @@
 #include <optional>
 #include <iostream>
 #include<vector>
+#include<math.h>
+
+sf::Vector2f NormalizeVector(sf::Vector2f vector)
+{
+    float m = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+    
+    sf::Vector2f normalizedVector;
+
+   normalizedVector.x = vector.x /m;
+   normalizedVector.y = vector.y /m;
+
+   return normalizedVector;
+
+
+}
 
 int main() {
 
@@ -17,7 +32,17 @@ int main() {
     //  sf::RenderWindow window(sf::VideoMode({600, 400}), "Dungeon Crawler Carl");
 
  //----------------INITIALISE-------- 
- 
+
+std::vector<sf::RectangleShape> projectiles; //plural
+
+// sf:: RectangleShape projectile(sf::Vector2f({10,5}));
+// projectile.setPosition(sf::Vector2f({50,50})); //dont set position in loop, only set once
+
+float projectileSpeed = 0.5f;
+sf::Vector2f projectileDirection;
+
+
+
  // -----------LOAD --------
 
 sf::Texture carlTexture;
@@ -34,10 +59,13 @@ else{
 sf::Sprite carlSprite(carlTexture);
 carlSprite.setTexture(carlTexture);
 
+
 // int Xindex = 0;
 // int Yindex = 0;
 // {x, Y}, {Width, Height}
 carlSprite.setTextureRect(sf::IntRect({0, 128}, {64, 64}));
+
+carlSprite.setPosition({100,700});
 
 sf::Texture donutTexture;
 
@@ -58,7 +86,20 @@ donutSprite.setPosition(sf::Vector2f(400,100));
 donutSprite.setTextureRect(sf::IntRect({96, 0}, {32, 32}));
 
  //----------LOAD-------
+
+//  projectile.setPosition(carlSprite.getPosition());
+
+//  //----projectile direction
+
+//  sf::Vector2f projectileDirection = donutSprite.getPosition() - projectile.getPosition();
+
+//  projectileDirection = NormalizeVector(projectileDirection);
+
+
 //----------------UPDATE---------    
+
+
+//----------------main game loop---------    
     while (window.isOpen()) {
 
 
@@ -68,42 +109,69 @@ donutSprite.setTextureRect(sf::IntRect({96, 0}, {32, 32}));
             
             }
         
+    
+
             
         }
+
+        // projectile.setPosition(projectile.getPosition() + projectileDirection * projectileSpeed);
+
          sf::Vector2f position = carlSprite.getPosition();
 
              if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
                 carlSprite.setTextureRect(sf::IntRect({64, 64 *3}, {64,64}));
-                carlSprite.setPosition(position + sf::Vector2f(.05f, 0.f));}
+                carlSprite.setPosition(position + sf::Vector2f(.5f, 0.f));}
             
             
              if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
                 
                 
                 carlSprite.setTextureRect(sf::IntRect({0, 64}, {64,64}));
-                carlSprite.setPosition(position + sf::Vector2f(-.05f, 0.f));
+                carlSprite.setPosition(position + sf::Vector2f(-.5f, 0.f));
 
              }
             
 
              if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
              {  carlSprite.setTextureRect(sf::IntRect({0, 0}, {64,64}));
-                carlSprite.setPosition(position + sf::Vector2f(0.f, -.05f));
+                carlSprite.setPosition(position + sf::Vector2f(0.f, -.5f));
               
              }
 
              if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)){
                 carlSprite.setTextureRect(sf::IntRect({0, 128}, {64,64}));
-                carlSprite.setPosition(position + sf::Vector2f(0.f, .05f));
+                carlSprite.setPosition(position + sf::Vector2f(0.f, .5f));
              }
+    
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            projectiles.push_back(sf::RectangleShape(sf::Vector2f({50,25}))); //add to vector
+
+            int i = projectiles.size() -1;
             
+            projectiles[i].setPosition(carlSprite.getPosition());
+        }
+
+        for(size_t i = 0; i < projectiles.size(); i++)
+        {
+            projectileDirection = donutSprite.getPosition() - projectiles[i].getPosition();
+            projectileDirection = NormalizeVector(projectileDirection);
+            projectiles[i].setPosition(projectiles[i].getPosition()+ projectileDirection * projectileSpeed);
+        }
+
+
 //----------------UPDATE---------  
 
 //----------------DRAW---------  
         window.clear(sf::Color::Black);
-        // drawing code goes here, shouldnt update here
+        // drawing code goes here, shouldn't update here
         window.draw(carlSprite);
         window.draw(donutSprite);
+        for (size_t i = 0; i <projectiles.size(); i ++){
+            window.draw(projectiles[i]);
+        }
+        // window.draw(projectile);
         window.display();
 //----------------DRAW---------  
     }
